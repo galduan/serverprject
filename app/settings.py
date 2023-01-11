c721cad5a68dd41470659631459b40e9bdef0e94
gal_duan <galduan99@gmail.com>
2023-01-11 19:23:27 +0200

    heroku

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,6 @@ with open(os.path.join(BASE_DIR, 'app/password_requirements.json')) as f:
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-o_xwy*%0-q-(9=9rwnp0e*!nxfz+k@ps8y1trwpyhh*v*10e8c'
 
 DEBUG = True
